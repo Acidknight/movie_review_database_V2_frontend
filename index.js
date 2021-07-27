1cb29baa8ee213e47e4e35b1f4c635771f033648
Anthony Leon <Acidknights89@gmail.com>
2021-07-27 18:27:30 -0400

    Start adding render logic to movies

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ function getMovies() {
     fetch(endPoint)
     .then(response => response.json())
     .then(movies => {
-        console.log(movies);
+        movies.data.forEach(movie => {
+            const movieMarkup = `
+                <div data-id=${movie.id}>
+                <img src=${movie.attributes.image_url}
+                height="200" width="250"
+                <h3>${movie.attributes.title}</h3>
+                <p>${movie.attributes.description}</p>
+                <p>${movie.attributes.release_year}</p>
+                <p>${movie.attributes.starring_actors}</p>
+                <p>${movie.attributes.genre.name}</p>
+                <button data-id-${movie.id}>edit</button>
+            </div>
+            <br></br>` ;
+        })
     })
 }
+
