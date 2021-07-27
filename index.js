547fdca72a716c68245b7c43b73dcf6e02e4ffba
Anthony Leon <Acidknights89@gmail.com>
2021-07-27 17:40:24 -0400

    Add movies index fetch function

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const endPoint = "http://localhost:3000/api/v1/movies"
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("loaded")
+    getMovies()
 })
+
+function getMovies() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(movies => {
+        console.log(movies);
+    })
+}
