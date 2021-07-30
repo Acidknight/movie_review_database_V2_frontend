4614e977de45c34b1120e13b3560fc01f1de4d5b
Anthony Leon <Acidknights89@gmail.com>
2021-07-30 01:54:39 -0400

    Add movie attributes to constructor and push movies into array

--- src/movie.js
+++ src/movie.js
@@ -1,6 +1,15 @@
 class Movie {
 
-    constructor({
-        
-    })
+    constructor(movie, movieAttributes) {
+        this.id = movie.id
+        this.title = movieAttributes.title
+        this.release_year = movieAttributes.release_year
+        this.description = movieAttributes.description
+        this.image_url = movieAttributes.image_url
+        this.starring_actors = movieAttributes.starring_actors
+        Movie.all.push(this)
+    }
+
 }
+
+Movie.all = [];
