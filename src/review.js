dc3f01a7ac52ddaf8eac4c651c6f8fa57d1938d7
Anthony Leon <Acidknights89@gmail.com>
2021-07-30 23:11:28 -0400

    Change attribute names

--- src/review.js
+++ src/review.js
@@ -1,5 +1,5 @@
 class Review {
-    constructor(movie_id, title, body){
+    constructor(movie_id, review_title, review_body){
     this.movie_id = movie_id
     this.review_title = title
     this.review_body = body
